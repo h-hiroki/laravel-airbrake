1499a76b6ef11964bb3e931035cc5cb96859c039
Adam Tester <adam.tester@vevvu.co.uk>
2016-05-31 15:29:04 +0100

    Tidy config

--- config/airbrake.php
+++ config/airbrake.php
@@ -12,9 +12,9 @@ return [
     ],
 
     // API Key
-    'id'  => env('AIRBRAKE_PROJECT_ID', ''),
+    'id' => env('AIRBRAKE_PROJECT_ID', ''),
     'key' => env('AIRBRAKE_API_KEY', ''),
 
     // Connection to the airbrake server
-    'host'      => 'api.airbrake.io',
+    'host' => 'api.airbrake.io',
 ];
