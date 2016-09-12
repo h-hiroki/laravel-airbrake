4e4ecdd62e8ef25c11805affd0b486ad806d9757
Tomahock <eu@tomahock.com>
2016-09-12 14:37:48 +0100

    get host from env or default

--- config/airbrake.php
+++ config/airbrake.php
@@ -16,5 +16,5 @@ return [
     'key' => env('AIRBRAKE_API_KEY', ''),
 
     // Connection to the airbrake server
-    'host' => 'api.airbrake.io',
+    'host' => env('AIRBRAKE_PROJECT_ID', 'api.airbrake.io'),
 ];
